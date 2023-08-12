342b68ef4892c675b92ed50c38defcfbd9b6c950
João Costa <joaocostagr@outlook.pt>
2023-08-12 19:31:41 +0100

    cleaning rep

---
+++ CursoEmVideo/NetBeansProjects/CursoEmVideo/src/Aula03/src/Aula02/Aula02.java
@@ -0,0 +1,22 @@
+package Aula02;
+
+/**
+ *
+ * @author jcosta
+ */
+public class Aula02 {
+
+    public static void main(String[] args) {
+        //Caneta 1
+        System.out.println("CANETA 1\n");
+        Caneta c1 = new Caneta();
+        c1.estadoCaneta();
+        c1.modelo = "bic cristal";
+        c1.cor = "azul";
+        // c1.ponta = 0.5;
+        //c1.tampada = false;
+        
+        
+    }
+    
+}
